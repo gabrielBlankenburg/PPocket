4c8092a28f585f6c0c90a984181e510d899ff505
Fernando Moreira <fe-ca-el@hotmail.com>
2017-12-05 10:00:03 +0000

    Add: Chatbot

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -17,5 +17,14 @@
     </script>
     <script src="<?= base_url(); ?>assets/script/jquery.mask.min.js"></script>
     <script src="<?= base_url(); ?>assets/script/main.js"></script>
+    <!-- Chatbot -->
+    <script>
+      !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+      arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+      d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+      insertBefore(d,q)}(window,document,'script','_gs');
+    
+      _gs('GSN-033615-C');
+    </script>
 </body>
 </html>
